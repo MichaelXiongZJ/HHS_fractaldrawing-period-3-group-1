5fa9bd3d97f5181465054f1d9918bf4280913d5f
Animan Patil <56457146+apatil987@users.noreply.github.com>
2021-03-18 13:37:45 -0700

    Update DragonCurve.java

--- FractalDrawing/src/DragonCurve.java
+++ FractalDrawing/src/DragonCurve.java
@@ -1,7 +1,7 @@
 import processing.core.PApplet;
 
 public class DragonCurve extends FractalDrawing{
-
+	private double r = 1.0/Math.sqrt(2.0);
 	public DragonCurve(int level, double length) {
 		super(level, length);
 	}
@@ -14,6 +14,9 @@ public class DragonCurve extends FractalDrawing{
 		if(level<1) {
 			marker.line((float)(x), (float)y, 
     				(float)(x+length*Math.cos(Math.toRadians(angle))), (float)(y-length*Math.sin(Math.toRadians(angle))));
+		}
+		else {
+			drawDragonCurve(marker,level-1,length*r,45,x,y);
 		}
 	}
 
