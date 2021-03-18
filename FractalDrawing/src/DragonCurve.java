44fa562775bebc546ebbac01999a5afaee902790
Animan Patil <56457146+apatil987@users.noreply.github.com>
2021-03-18 13:23:19 -0700

    Create DragonCurve.java

---
+++ FractalDrawing/src/DragonCurve.java
@@ -0,0 +1,16 @@
+import processing.core.PApplet;
+
+public class DragonCurve extends FractalDrawing{
+
+	public DragonCurve(int level, double length) {
+		super(level, length);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void draw(PApplet marker) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
