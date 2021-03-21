332e1f9162837a0d1815103ed7f5677758e7c0ff
LakshyaShrivastava <lshrivastava169@student.fuhsd.org>
2021-03-21 11:24:43 -0700

    Created Sierpinski Triangle class

---
+++ FractalDrawing/src/SierpinskiTriangle.java
@@ -0,0 +1,14 @@
+import processing.core.PApplet;
+
+public class SierpinskiTriangle extends FractalDrawing{
+
+	public SierpinskiTriangle(int level, double length) {
+		super(level, length);
+	}
+
+	@Override
+	public void draw(PApplet marker) {
+		
+	}
+
+}
