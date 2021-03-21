db4c90f80f0268fe5d335acf6d6c1df06cf75ab6
LakshyaShrivastava <lshrivastava169@student.fuhsd.org>
2021-03-21 11:52:26 -0700

    Added a Coordinates helper class. Updated sierpenskiTriangle class

--- FractalDrawing/src/SierpinskiTriangle.java
+++ FractalDrawing/src/SierpinskiTriangle.java
@@ -2,13 +2,41 @@ import processing.core.PApplet;
 
 public class SierpinskiTriangle extends FractalDrawing{
 
-	public SierpinskiTriangle(int level, double length) {
+	private Coordinates pointA, pointB, pointC;
+	public SierpinskiTriangle(int level, double length, Coordinates pointA, Coordinates pointB, Coordinates pointC) 
+	{
 		super(level, length);
+		this.pointA = pointA;
+		this.pointB = pointB;
+		this.pointC = pointC;
 	}
 
-	@Override
-	public void draw(PApplet marker) {
+	public void draw(PApplet marker) 
+	{
+		drawSierpinskiTriangle(marker, pointA, pointB, pointC, level);
+	}
+
+	private void drawSierpinskiTriangle(PApplet marker, Coordinates pointA, Coordinates pointB, Coordinates pointC, int level)
+	{
+		if(level < 1)
+		{
+			marker.triangle(pointA.getX(), pointA.getY(), pointB.getX(), pointB.getY(), pointC.getX(), pointC.getY());
+			return;
+		}
 		
+			Coordinates pointAB = getMidPoint(pointA,pointB);
+			Coordinates pointBC = getMidPoint(pointB,pointC);
+			Coordinates pointCA = getMidPoint(pointC,pointA);
+			
+			drawSierpinskiTriangle(marker, pointA, pointAB, pointCA, level-1);
+			drawSierpinskiTriangle(marker, pointAB, pointB, pointBC, level-1);
+			drawSierpinskiTriangle(marker, pointCA, pointBC, pointC, level-1);
 	}
-
+	
+	private Coordinates getMidPoint(Coordinates pointA, Coordinates pointB)
+	{
+		float midX = (pointA.getX() + pointB.getX()) / 2;
+		float midY = (pointA.getY()) + pointB.getY()/2;
+		return new Coordinates(midX, midY);
+	}
 }
