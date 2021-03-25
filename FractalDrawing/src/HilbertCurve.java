dacb0a21c3bdd68f1675ca59e6c4707675920d6e
Zejun Xiong <69989842+MichaelThePanda@users.noreply.github.com>
2021-03-25 13:23:55 -0700

    Update HilbertCurve.java

--- FractalDrawing/src/HilbertCurve.java
+++ FractalDrawing/src/HilbertCurve.java
@@ -4,6 +4,7 @@ public class HilbertCurve extends FractalDrawing{
 
 	private double xLoc;
 	private double yLoc;
+	private float color;
 	
 	public HilbertCurve(int level, double length) {
 		super(level, length);
@@ -13,12 +14,15 @@ public class HilbertCurve extends FractalDrawing{
 
 	
 	public void draw(PApplet marker) {
+		color = 0;
 		drawHilbertCurve(marker, xLoc, yLoc, super.length, super.level, 0);
 	}
 	
 	private void drawHilbertCurve(PApplet marker, double x, double y, double length, int level, double angle) {
 		if(angle >= 360) {
 			angle -= 360;
+		}else if(angle<0) {
+			angle += 360;
 		}
 		if(level < 1) {
 			drawCup(marker, x, y, length, level, angle);
@@ -26,6 +30,8 @@ public class HilbertCurve extends FractalDrawing{
 			length = length*3/2;
 			x = x-length/6;
 			y = y-length/6;
+			marker.stroke(color, color/100, color/100);;
+			color += 1;
 			if(angle == 0) {
 				drawHilbertCurve(marker, x, y, length/3, level - 1, angle + 90);
 				drawHilbertCurve(marker, x, y+2*length/3, length/3, level - 1, angle);
