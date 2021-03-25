9d74abd196687b1d2fbd1d0fc48bf1e4dcd67bad
Zejun Xiong <69989842+MichaelThePanda@users.noreply.github.com>
2021-03-25 00:02:25 -0700

    Update HilbertCurve.java

--- FractalDrawing/src/HilbertCurve.java
+++ FractalDrawing/src/HilbertCurve.java
@@ -29,21 +29,33 @@ public class HilbertCurve extends FractalDrawing{
 				drawHilbertCurve(marker, x, y+2*length/3, length/3, level - 1, angle);
 				drawHilbertCurve(marker, x+2*length/3, y+2*length/3 , length/3, level - 1, angle);
 				drawHilbertCurve(marker, x+2*length/3 , y, length/3, level - 1, angle + 270);
+				marker.line((float)x, (float)(y+length/3), (float)x, (float)(y+2*length/3));
+				marker.line((float)(x+length/3), (float)(y+2*length/3), (float)(x+2*length/3), (float)(y+2*length/3));
+				marker.line((float)(x+length), (float)(y+length/3), (float)(x+length), (float)(y+2*length/3));
 			}else if(angle == 90) {
 				drawHilbertCurve(marker, x , y , length/3, level - 1, angle - 90);
 				drawHilbertCurve(marker, x+2*length/3, y, length/3, level - 1, angle);
 				drawHilbertCurve(marker, x+2*length/3, y+2*length/3 , length/3, level - 1, angle);
 				drawHilbertCurve(marker, x, y+2*length/3, length/3, level - 1, angle + 90);
+				marker.line((float)(x+length/3), (float)y, (float)(x+2*length/3), (float)y);
+				marker.line((float)(x+2*length/3), (float)(y+length/3), (float)(x+2*length/3), (float)(y+2*length/3));
+				marker.line((float)(x+length/3), (float)(y+length), (float)(x+2*length/3), (float)(y+length));
 			}else if(angle == 180) {
 				drawHilbertCurve(marker, x , y , length/3, level - 1, angle);
 				drawHilbertCurve(marker, x+2*length/3, y, length/3, level - 1, angle);
 				drawHilbertCurve(marker, x, y+2*length/3 , length/3, level - 1, angle - 90);
 				drawHilbertCurve(marker, x+2*length/3, y+2*length/3, length/3, level - 1, angle + 90);
+				marker.line((float)x, (float)(y+length/3), (float)x, (float)(y+2*length/3));
+				marker.line((float)(x+length/3), (float)(y+length/3), (float)(x+2*length/3), (float)(y+length/3));
+				marker.line((float)(x+length), (float)(y+length/3), (float)(x+length), (float)(y+2*length/3));
 			}else if(angle == 270) {
 				drawHilbertCurve(marker, x+2*length/3 , y , length/3, level - 1, angle + 90);
-				drawHilbertCurve(marker, x, y, length/3, level - 1, angle-180);
-				drawHilbertCurve(marker, x, y+2*length/3, length/3, level - 1, angle-180);
+				drawHilbertCurve(marker, x, y, length/3, level - 1, angle);
+				drawHilbertCurve(marker, x, y+2*length/3, length/3, level - 1, angle);
 				drawHilbertCurve(marker, x+2*length/3, y+2*length/3, length/3, level - 1, angle - 90);
+				marker.line((float)(x+length/3), (float)y, (float)(x+2*length/3), (float)y);
+				marker.line((float)(x+length/3), (float)(y+length/3), (float)(x+length/3), (float)(y+2*length/3));
+				marker.line((float)(x+length/3), (float)(y+length), (float)(x+2*length/3), (float)(y+length));
 			}
 		}
 	}
