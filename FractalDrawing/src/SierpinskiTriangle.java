f701a8852579011bbea36fc3ee7acf42ec3966d6
LakshyaShrivastava <lshrivastava169@student.fuhsd.org>
2021-04-01 09:05:39 -0700

    sierpinski triangle now uses length as well

--- FractalDrawing/src/SierpinskiTriangle.java
+++ FractalDrawing/src/SierpinskiTriangle.java
@@ -1,8 +1,12 @@
-import processing.core.PApplet;
+import java.awt.Color;
 
-public class SierpinskiTriangle extends FractalDrawing{
+import processing.core.PApplet;
+import shrivastava.shapes.Line;
+
+public class SierpinskiTriangle extends FractalDrawing {
 
 	private Coordinates pointA, pointB, pointC;
+
 	public SierpinskiTriangle(int level, double length, Coordinates pointA, Coordinates pointB, Coordinates pointC) 
 	{
 		super(level, length);
@@ -13,10 +17,46 @@ public class SierpinskiTriangle extends FractalDrawing{
 
 	public void draw(PApplet marker) 
 	{
-		
-		drawSierpinskiTriangle(marker, pointA, pointB, pointC, level);
+//		drawSierpinskiTriangle(marker, pointA, pointB, pointC, level); // this method cannot use length but otherwise works
+		drawSierpinskiTriangle(marker, pointA, pointB, pointC, level, length); 
 	}
 
+	private void drawSierpinskiTriangle(PApplet marker, Coordinates pointA, Coordinates pointB, Coordinates pointC, int level, double length)
+	{
+		if(level < 1)
+		{
+//			marker.fill((float)Math.random() * 255,(float)Math.random() * 255, (float)Math.random() * 255);
+			marker.stroke((float)Math.random() * 255,(float)Math.random() * 255, (float)Math.random() * 255);
+//			marker.triangle(pointA.getX(), pointA.getY(), pointB.getX(), pointB.getY(), pointC.getX(), pointC.getY());
+			Line[] triangle = new Line[3];
+			triangle[0] = new Line (pointA.getX(), pointA.getY(), -60, length, true);
+			triangle[1] = new Line(triangle[0].getX2(), triangle[0].getY2(), 60, length, true);
+			triangle[2] = new Line(pointA.getX(), pointA.getY(), 0, length, true);
+			
+			for(Line l : triangle)
+			{
+				l.setStrokeColor(new Color(30,100, 200));
+				l.draw(marker);
+			}
+			return;
+		}
+		
+			Coordinates pointAB = getMidPoint(pointA,length, -60);
+			Coordinates pointBC = getMidPoint(pointB,length, 60);
+			Coordinates pointAC = getMidPoint(pointA,length, 0);
+			
+			drawSierpinskiTriangle(marker, pointA, pointAB, pointAC, level-1, length/2);
+			drawSierpinskiTriangle(marker, pointAB, pointB, pointBC, level-1, length/2);
+			drawSierpinskiTriangle(marker, pointAC, pointBC, pointC, level-1, length/2);
+	}
+	
+	private Coordinates getMidPoint(Coordinates pointA, double length, int angle)
+	{
+		Line l = new Line(pointA.getX(), pointA.getY(), angle, length, true);
+	return  getMidPoint(new Coordinates((float)l.getX(),(float) l.getY()), new Coordinates((float)l.getX2(), (float)l.getY2())) ;
+	}
+	
+//	doesnt use length but has fill color 
 	private void drawSierpinskiTriangle(PApplet marker, Coordinates pointA, Coordinates pointB, Coordinates pointC, int level)
 	{
 		if(level < 1)
@@ -24,6 +64,7 @@ public class SierpinskiTriangle extends FractalDrawing{
 			marker.fill((float)Math.random() * 255,(float)Math.random() * 255, (float)Math.random() * 255);
 			marker.stroke((float)Math.random() * 255,(float)Math.random() * 255, (float)Math.random() * 255);
 			marker.triangle(pointA.getX(), pointA.getY(), pointB.getX(), pointB.getY(), pointC.getX(), pointC.getY());
+			
 			return;
 		}
 		
@@ -35,11 +76,13 @@ public class SierpinskiTriangle extends FractalDrawing{
 			drawSierpinskiTriangle(marker, pointAB, pointB, pointBC, level-1);
 			drawSierpinskiTriangle(marker, pointAC, pointBC, pointC, level-1);
 	}
-	
-	private Coordinates getMidPoint(Coordinates pointA, Coordinates pointB)
+
+	private Coordinates getMidPoint(Coordinates pointA, Coordinates pointB) 
 	{
 		float midX = (pointA.getX() + pointB.getX()) / 2;
-		float midY = (pointA.getY() + pointB.getY())/2;
+		float midY = (pointA.getY() + pointB.getY()) / 2;
 		return new Coordinates(midX, midY);
 	}
+	
+	
 }
